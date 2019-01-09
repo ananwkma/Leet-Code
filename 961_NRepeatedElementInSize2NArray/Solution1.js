22efc539155b5b1931f3daeb3fd6a0f0b0c70b9b
Anan <anan.w.ma@gmail.com>
2019-01-08 19:12:14 -0800

    961 Solution1
    - My solution to problem 961 added

--- 961_NRepeatedElementInSize2NArray/Solution1.js
+++ 961_NRepeatedElementInSize2NArray/Solution1.js
@@ -1,7 +1,15 @@
-/**
+/*
+ * https://leetcode.com/problems/n-repeated-element-in-size-2n-array/?fbclid=IwAR393WnPoGLjC_m6F9YKCvweQwaf6siUKDPfTL-XJ2XkmmUG9dcJQjt53fo
  * @param {number[]} A
  * @return {number}
  */
 var repeatedNTimes = function(A) {
-    
+    let uniqueNums = new Set()
+    for (var i = 0; i < A.length; i++) {
+    	if (uniqueNums.has(A[i])) return A[i]
+        else uniqueNums.add(A[i])
+    }
 };
+
+let test = [1,2,3,3]
+console.log('Expected : 3' + '\n' + 'Output: ' + repeatedNTimes(test))
