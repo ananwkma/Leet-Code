4faff9e3a31a6cc5917686a6ff5e542f16923aa3
Anan <anan.w.ma@gmail.com>
2019-01-11 19:15:36 -0800

    874 Solution1 Half Done
    - Movement for robot done
    - Need to implement obstacles

--- 874_WalkingRobotSimulation/Solution1.js
+++ 874_WalkingRobotSimulation/Solution1.js
@@ -9,8 +9,49 @@
  * Space: O()
  */
 
+changeDirection = (command, direction) => {
+	if (command === -1) {
+    	direction -= 1
+    	if (direction < 1) direction = 4
+    }
+    else if (command === -2) {
+    	direction += 1
+    	if (direction > 4) direction = 1
+    }
+	return direction
+}
+
+checkObstacles = (obstacles, position, direction, command) => {
+
+}
+
 var robotSim = function(commands, obstacles) {
-    
+    let direction = 1 // 1: North, 2: East, 3: South, 4: West
+    let position = [0, 0]
+    commands.map(command => {
+    	if (command > 0) {
+	    	switch (direction) {
+		    	case 1:
+		    		position[1] += command
+		    		break
+		    	case 2:
+		    		position[0] += command
+		    		break
+		    	case 3:
+		    		position[1] -= command
+		    		break
+		    	case 4:
+		    		position[0] -= command
+		    		break
+		    	default: 
+		    		break
+	    	}
+	    }
+	    else direction = changeDirection(command, direction)
+    }) 
+    console.log(position)
+    return Math.pow(position[0], 2) + Math.pow(position[1], 2)
 };
 
-console.log('Expected: 0' + '\n' + 'Output: ' + robotSim([1,2,3,4,5],-1))
+console.log('Expected: 25' + '\n' + 'Output: ' + robotSim([4,-1,3],[]))
+//console.log('Expected: 65' + '\n' + 'Output: ' + robotSim([4,-1,4,-2,4],[2,4]))
