d8e82881794bf950f914d47c952d55455b71d55d
Anan <anan.w.ma@gmail.com>
2019-01-11 17:54:10 -0800

    532 Solution1
    - solution1 added for problem 532

--- 532_KDiffPairs/Solution1.js
+++ 532_KDiffPairs/Solution1.js
@@ -5,12 +5,24 @@
  * @param {number} k
  * @return {number}
  *
- * Time: O()
- * Space: O()
+ * Time: O(N), O(Nlog(N)) with sort
+ * Space: O(N)
  */
 
 var findPairs = function(nums, k) {
-    
+	if (k < 0) return 0
+	// nums.sort(function(a,b) { return a - b })
+	let allNums = new Set()
+	let resNums = new Set()
+	for (let i = 0; i < nums.length; i++) {
+		if (allNums.has(nums[i] - k)) resNums.add(nums[i])
+		if (allNums.has(nums[i] + k)) resNums.add(nums[i] + k)
+		allNums.add(nums[i])
+	}
+	return resNums.size
 };
 
+console.log('Expected: 0' + '\n' + 'Output: ' + findPairs([1,2,3,4,5],-1))
+console.log('Expected: 2' + '\n' + 'Output: ' + findPairs([3,3,4,1,5],2))
 console.log('Expected: 2' + '\n' + 'Output: ' + findPairs([3,1,4,1,5],2))
+console.log('Expected: 1' + '\n' + 'Output: ' + findPairs([1,3,1,5,4],0))
---
+++ 874_WalkingRobotSimulation/Solution1.js
@@ -0,0 +1,16 @@
+/*
+ * https://leetcode.com/problems/walking-robot-simulation/
+ *
+ * @param {number[]} commands
+ * @param {number[][]} obstacles
+ * @return {number}
+ *
+ * Time: O()
+ * Space: O()
+ */
+
+var robotSim = function(commands, obstacles) {
+    
+};
+
+console.log('Expected: 0' + '\n' + 'Output: ' + robotSim([1,2,3,4,5],-1))
