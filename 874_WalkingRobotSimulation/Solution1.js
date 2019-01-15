48bd5d5ad3512669d9cbb714df7574c8a5fc1356
Anan <anan.w.ma@gmail.com>
2019-01-14 19:36:41 -0800

    874 Solution2
    - 874 Solution1 abandoned
    - 874 Solution2 added
    - More efficient way to check and modify coordinates
    - Check obstacles with set

--- 874_WalkingRobotSimulation/Solution1.js
+++ 874_WalkingRobotSimulation/Solution1.js
@@ -7,22 +7,90 @@
  *
  * Time: O()
  * Space: O()
+ *
+ * ATTEMPT 1
  */
 
 changeDirection = (command, direction) => {
-	if (command === -1) {
-    	direction -= 1
-    	if (direction < 1) direction = 4
-    }
-    else if (command === -2) {
-    	direction += 1
-    	if (direction > 4) direction = 1
-    }
-	return direction
+	if (command === -1) return (direction = (direction + 1) % 4)
+    else if (command === -2) return (direction = (direction + 3) % 4)
 }
 
-checkObstacles = (obstacles, position, direction, command) => {
+noObstacles = (direction, position, command) => {
+	switch (direction) {
+    	case 1: // North
+    		position[1] += command
+    		break
+    	case 2: // East
+    		position[0] += command
+    		break
+    	case 3: // South
+    		position[1] -= command
+    		break
+    	case 4: // West
+    		position[0] -= command
+    		break
+    	default: 
+    		break
+	}
+}
 
+checkObstacles = (direction, position, command, obstacles) => {
+	let blocked = false
+	let closestObs = obstacles[0]
+	switch (direction) {
+    	case 1: // North
+    		obstacles.map(obstacle => {
+    			if (obstacle[0] === position[0] 
+    			&& position[1] < obstacle[1] < (position[1] += command))
+					blocked = true
+    				closestObs = compareObstacles(obstacle, position, closestObs)
+    		})
+    		if (blocked) position[1] = closestObs[1] - 1
+    		else position[1] += command
+    		break
+    	case 2: // East
+    		obstacles.map(obstacle => {
+    			if (obstacle[1] === position[1] 
+    			&& position[1] < obstacle[1] < (position[0] += command))
+    				blocked = true
+    				closestObs = compareObstacles(obstacle, position, closestObs)
+    		})
+    		if (blocked) position[0] = closestObs[0] - 1
+    		else position[0] += command
+    		break
+    	case 3: // South
+    		obstacles.map(obstacle => {
+    			if (obstacle[0] === position[0] 
+    			&& position[1] > obstacle[1] > (position[1] -= command))
+    				blocked = true
+    				closestObs = compareObstacles(obstacle, position, closestObs)
+    		})
+    		if (blocked) position[1] = closestObs[1] + 1
+    		else position[1] -= command
+    		break
+    	case 4: // West
+    		obstacles.map(obstacle => {
+    			if (obstacle[1] === position[1] 
+    			&& position[1] > obstacle[1] > (position[0] -= command))
+    				blocked = true
+    				closestObs = compareObstacles(obstacle, position, closestObs)
+    		})
+    		if (blocked) position[0] = closestObs[0] + 1
+    		else position[0] -= command
+    		break
+    	default: 
+    		break
+	}
+	console.log('closestObs', closestObs)
+	console.log('pos', position)
+}
+
+compareObstacles = (obstacle, position, closestObs) => {
+	if (Math.abs(obstacle[0] + obstacle[1] - position[0] - position[1])
+	< Math.abs(closestObs[0] + closestObs[1] - position[0] - position[1]))
+		return obstacle
+	else return closestObs
 }
 
 var robotSim = function(commands, obstacles) {
@@ -30,28 +98,14 @@ var robotSim = function(commands, obstacles) {
     let position = [0, 0]
     commands.map(command => {
     	if (command > 0) {
-	    	switch (direction) {
-		    	case 1:
-		    		position[1] += command
-		    		break
-		    	case 2:
-		    		position[0] += command
-		    		break
-		    	case 3:
-		    		position[1] -= command
-		    		break
-		    	case 4:
-		    		position[0] -= command
-		    		break
-		    	default: 
-		    		break
-	    	}
+	    	if (obstacles.length > 0) checkObstacles(direction, position, command, obstacles)
+	    	else noObstacles(direction, position, command)
 	    }
 	    else direction = changeDirection(command, direction)
     }) 
-    console.log(position)
     return Math.pow(position[0], 2) + Math.pow(position[1], 2)
 };
 
-console.log('Expected: 25' + '\n' + 'Output: ' + robotSim([4,-1,3],[]))
-//console.log('Expected: 65' + '\n' + 'Output: ' + robotSim([4,-1,4,-2,4],[2,4]))
+// console.log('Expected: 25' + '\n' + 'Output: ' + robotSim([4,-1,3],[]))
+// console.log('Expected: 65' + '\n' + 'Output: ' + robotSim([4,-1,4,-2,4],[[2,4]]))
+console.log('Expected: 32' + '\n' + 'Output: ' + robotSim([4,-1,4,-2,4],[[2,4],[0,1]]))
