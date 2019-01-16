e780888554a8f7a146c245ada876cd7fcc1b788d
Anan <anan.w.ma@gmail.com>
2019-01-15 21:30:38 -0800

    168 Solution1
    - solution1 for 168 added

--- 168_ExcelSheetColumnTitle/Solution1.js
+++ 168_ExcelSheetColumnTitle/Solution1.js
@@ -5,13 +5,23 @@
  * @return {string}
  *
  * Time: O()
- * Space: O()
+ * Space: O(1)
  */
 
 var convertToTitle = function(n) {
-    
+    let answer = ''
+    while (n > 0) {
+    	let remainder = (n % 26)
+    	if (remainder === 0) remainder = 26
+    	answer = String.fromCharCode(remainder + 64) + answer
+    	n = (n - remainder) / 26
+    }
+    return answer
 };
 
 console.log('Expected: A' + '\n' + 'Output: ' + convertToTitle(1))
 console.log('Expected: AB' + '\n' + 'Output: ' + convertToTitle(28))
 console.log('Expected: ZY' + '\n' + 'Output: ' + convertToTitle(701))
+
+
+
---
+++ 414_ThirdMaximumNumber/Solution1.js
@@ -0,0 +1,15 @@
+/*
+ * https://leetcode.com/problems/third-maximum-number/
+ *
+ * @param {number[]} nums
+ * @return {number}
+ *
+ * Time: O()
+ * Space: O()
+ */
+
+var thirdMax = function(nums) {
+    
+};
+
+console.log('Expected: A' + '\n' + 'Output: ' + thirdMax(1))
