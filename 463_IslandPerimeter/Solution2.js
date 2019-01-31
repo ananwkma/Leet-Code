20a550e4bf1802cc59d3db7328b73a5e748e4823
Anan <anan.w.ma@gmail.com>
2019-01-30 17:09:19 -0800

    463 Solution1 and 2
    - Solution1 modular Solution2 condensed

--- 463_IslandPerimeter/Solution2.js
+++ 463_IslandPerimeter/Solution2.js
@@ -8,20 +8,11 @@
  * Space: O(1)
  */
 
-numNeighbors = (grid, i, j) => {
-	let count = 0
-	if ((i+1 < grid.length) && grid[i+1][j] === 1) count++
-	if ((i-1 >= 0) && grid[i-1][j] === 1) count++
-	if ((j+1 < grid[0].length) && grid[i][j+1] === 1) count++
-	if ((j-1 >= 0) && grid[i][j-1] === 1) count++
-	return count
-}
-
 var islandPerimeter = function(grid) {
     let count = 0
     for (let i = 0; i < grid.length; i++) {
     	for (let j = 0; j < grid[0].length; j++) {
-    		if (grid[i][j] === 1) count += (4 - numNeighbors(grid, i, j))
+    		if (grid[i][j] === 1) count += (4 - grid[i+1][j] + grid[i-1][j] + grid[i][j+1] + grid[i][j-1])
     	}
     }
     return count
